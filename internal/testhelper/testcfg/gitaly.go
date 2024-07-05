37eac8a10ba4246ac230d2c980c28f97a02779e0
James Liu <jliu@gitlab.com>
2024-07-05 14:00:15 +1000

    test: Wire up bundled Git binaries
    When configuring the Git execution environment, we source compiled Git
    binaries from cfg.BinDir or GITALY_TESTING_BUNDLED_GIT_PATH if it's set.
    
    In production (on Omnibus for example), cfg.BinDir will point to
    /opt/gitlab/embedded/bin which contains shared binaries across the
    entire GitLab installation such as `gitlab-workhorse` and
    `gitaly-backup`. The Git binaries end up there because Omnibus executes
    our `build-bundled-git` Makefile target during installation. See
    https://gitlab.com/gitlab-org/omnibus-gitlab/-/blob/master/config/software/git.rb
    for more information.
    
    During testing, the Makefile sets GITALY_TESTING_BUNDLED_GIT_PATH to
    _build/bin in an effort to make the tests use the bundled Git
    environment. Unfortunately, if the tests are run without using the
    `make test...` target (such as in an IDE), the env var is not set and
    tests will fall-back to whatever Git is on the PATH.
    
    Simplify this logic by using cfg.RuntimeDir for both testing and
    production:
    
    - In testing, remove the GITALY_TESTING_BUNDLED_GIT_PATH env var.
      Instead, we can symlink the various Git binaries in _build/bin to
      cfg.RuntimeDir and issue a warning that fails the test if the Git
      binaries were not present in _build/bin.
    - In production, use cfg.RuntimeDir directly to source the Git binaries.
      We unpack the embedded Git binaries in packed_binaries.go into that
      directory on startup. Since Gitaly's RuntimeDir is independent of how
      Gitaly was packaged, we retain full control over the Git binary that
      we execute in production.

--- internal/testhelper/testcfg/gitaly.go
+++ internal/testhelper/testcfg/gitaly.go
@@ -46,6 +46,19 @@ func WithPackObjectsCacheEnabled() Option {
 	}
 }
 
+// WithDisableBundledGitSymlinking stops the builder from symlinking the
+// compiled bundled Git binaries in _build/bin to cfg.RuntimeDir. This is
+// useful for tests like cmd/gitaly/check_test.go which executes a test
+// against a compiled Gitaly binary. If this option is not supplied, both
+// the test and the Gitaly process it spawns will try to populate RuntimeDir
+// with the same binaries, which causes a "file exists" exception in
+// packed_binaries.go
+func WithDisableBundledGitSymlinking() Option {
+	return func(builder *GitalyCfgBuilder) {
+		builder.disableBundledGitSymlinking = true
+	}
+}
+
 // NewGitalyCfgBuilder returns gitaly configuration builder with configured set of options.
 func NewGitalyCfgBuilder(opts ...Option) GitalyCfgBuilder {
 	cfgBuilder := GitalyCfgBuilder{}
@@ -61,8 +74,9 @@ func NewGitalyCfgBuilder(opts ...Option) GitalyCfgBuilder {
 type GitalyCfgBuilder struct {
 	cfg config.Cfg
 
-	storages                []string
-	packObjectsCacheEnabled bool
+	storages                    []string
+	packObjectsCacheEnabled     bool
+	disableBundledGitSymlinking bool
 }
 
 // Build setups required filesystem structure, creates and returns configuration of the gitaly service.
@@ -100,6 +114,26 @@ func (gc *GitalyCfgBuilder) Build(tb testing.TB) config.Cfg {
 		cfg.RuntimeDir = filepath.Join(root, "runtime.d")
 		require.NoError(tb, os.Mkdir(cfg.RuntimeDir, perm.PrivateDir))
 		require.NoError(tb, os.Mkdir(cfg.InternalSocketDir(), perm.PrivateDir))
+
+		if !gc.disableBundledGitSymlinking {
+			// We do this symlinking to emulate what happens with the Git binaries in production:
+			// - In production, the Git binaries get unpacked by packed_binaries.go into Gitaly's RuntimeDir, which for
+			//   Omnibus is something like /var/opt/gitlab/gitaly/run/gitaly-<xxx>.
+			// - In testing, the binaries remain in the _build/bin directory of the repository root.
+			bundledGitBins, err := filepath.Glob(filepath.Join(testhelper.SourceRoot(tb), "_build", "bin", "gitaly-git-*"))
+			require.NoError(tb, err)
+
+			for _, targetPath := range bundledGitBins {
+				destinationPath := filepath.Join(cfg.RuntimeDir, filepath.Base(targetPath))
+
+				// It's possible that the same RuntimeDir is used across multiple invocations of
+				// testcfg.Build(), so we simply remove any existing symlinks.
+				if _, err := os.Lstat(destinationPath); err == nil {
+					require.NoError(tb, os.Remove(destinationPath))
+				}
+				require.NoError(tb, os.Symlink(targetPath, destinationPath))
+			}
+		}
 	}
 
 	if len(cfg.Storages) != 0 && len(gc.storages) != 0 {
