6bdda24229e7ce3ad6d525826c72d8e82daa1eb7
Sami Hiltunen <shiltunen@gitlab.com>
2024-07-10 17:54:03 +0300

    Remove production usage of GroupPrivateDir
    GroupPrivateDir is only used when creating the parent directory
    hierarchy of a repository. The storage contents of Gitaly don't need
    to be readable by anyone else than Gitaly. Replace the usage of
    GroupPrivateDir with standard storage permissions.

--- internal/gitaly/repoutil/lock.go
+++ internal/gitaly/repoutil/lock.go
@@ -6,7 +6,6 @@ import (
 	"path/filepath"
 
 	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage"
-	"gitlab.com/gitlab-org/gitaly/v16/internal/helper/perm"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/log"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/safe"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/structerr"
@@ -26,7 +25,7 @@ func Lock(ctx context.Context, logger log.Logger, locator storage.Locator, repos
 	}
 
 	// Create the parent directory in case it doesn't exist yet.
-	if err := os.MkdirAll(filepath.Dir(path), perm.GroupPrivateDir); err != nil {
+	if err := os.MkdirAll(filepath.Dir(path), storage.ModeDirectory.Perm()); err != nil {
 		return nil, structerr.NewInternal("create directories: %w", err)
 	}
 
