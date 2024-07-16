fd2431834a139ce96a5a3f602b6423134a6b8e93
Sami Hiltunen <shiltunen@gitlab.com>
2024-07-16 12:18:56 +0300

    Move mode's tests to external package
    Testhelper package will needs modes in some of its functions. This
    leads to an import cycle as mode package needs to import testhelper
    to call testhelper.Run in its TestMain as enforced by our linter.
    
    Move the mode package's tests to an external package to avoid the
    cycle. We need to do the same also for permission package as its
    tests also need to import testhelper which then again import mode
    and itself through there.

--- internal/gitaly/storage/mode/permission/permission_test.go
+++ internal/gitaly/storage/mode/permission/permission_test.go
@@ -1,43 +1,44 @@
-package permission
+package permission_test
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage/mode/permission"
 )
 
 func TestOwnerRead(t *testing.T) {
-	require.Equal(t, "-r--------", OwnerRead.String())
+	require.Equal(t, "-r--------", permission.OwnerRead.String())
 }
 
 func TestOwnerWrite(t *testing.T) {
-	require.Equal(t, "--w-------", OwnerWrite.String())
+	require.Equal(t, "--w-------", permission.OwnerWrite.String())
 }
 
 func TestOwnerExecute(t *testing.T) {
-	require.Equal(t, "---x------", OwnerExecute.String())
+	require.Equal(t, "---x------", permission.OwnerExecute.String())
 }
 
 func TestGroupRead(t *testing.T) {
-	require.Equal(t, "----r-----", GroupRead.String())
+	require.Equal(t, "----r-----", permission.GroupRead.String())
 }
 
 func TestGroupWrite(t *testing.T) {
-	require.Equal(t, "-----w----", GroupWrite.String())
+	require.Equal(t, "-----w----", permission.GroupWrite.String())
 }
 
 func TestGroupExecute(t *testing.T) {
-	require.Equal(t, "------x---", GroupExecute.String())
+	require.Equal(t, "------x---", permission.GroupExecute.String())
 }
 
 func TestOthersRead(t *testing.T) {
-	require.Equal(t, "-------r--", OthersRead.String())
+	require.Equal(t, "-------r--", permission.OthersRead.String())
 }
 
 func TestOthersWrite(t *testing.T) {
-	require.Equal(t, "--------w-", OthersWrite.String())
+	require.Equal(t, "--------w-", permission.OthersWrite.String())
 }
 
 func TestOthersExecute(t *testing.T) {
-	require.Equal(t, "---------x", OthersExecute.String())
+	require.Equal(t, "---------x", permission.OthersExecute.String())
 }
