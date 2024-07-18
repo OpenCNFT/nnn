f3fb12f831e33ef97b2221e37e627dd096f85fdb
John Cai <jcai@gitlab.com>
2024-07-17 20:48:26 -0400

    smarthttp: Generate bundle in background
    clone, check if 5 or more clones are already happening for this
    repository. If so, and a bundle does not exist, then we want to generate
    one because this is likely a busy repository.

--- internal/gitaly/service/smarthttp/upload_pack.go
+++ internal/gitaly/service/smarthttp/upload_pack.go
@@ -1,20 +1,33 @@
 package smarthttp
 
 import (
+	"bytes"
 	"context"
 	"crypto/sha1"
 	"errors"
 	"fmt"
 	"io"
+	"time"
 
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus/ctxlogrus"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/bundleuri"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/command"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/featureflag"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/git/localrepo"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git/stats"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage/storagectx"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage/storagemgr"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/grpc/sidechannel"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/log"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/structerr"
 	"gitlab.com/gitlab-org/gitaly/v16/proto/go/gitalypb"
+)
+
+const (
+	concurrentUploadPackThreshold = 5
+	bundleGenerationTimeout       = 24 * time.Hour
 )
 
 func (s *server) PostUploadPackWithSidechannel(ctx context.Context, req *gitalypb.PostUploadPackWithSidechannelRequest) (*gitalypb.PostUploadPackWithSidechannelResponse, error) {
@@ -117,20 +130,84 @@ func (s *server) runUploadPack(ctx context.Context, req *gitalypb.PostUploadPack
 
 	gitConfig = append(gitConfig, bundleuri.CapabilitiesGitConfig(ctx)...)
 
+	txID := storage.ExtractTransactionID(ctx)
+
+	var originalRepo *gitalypb.Repository
+
+	if txID != 0 {
+		currentTx, err := s.transactionRegistry.Get(txID)
+		if err != nil {
+			return nil, structerr.NewInternal("error getting transaction: %w", err)
+		}
+		originalRepo = currentTx.OriginalRepository(req.GetRepository())
+	} else {
+		originalRepo = req.GetRepository()
+	}
+
+	key := originalRepo.GetGlRepository()
+
 	uploadPackConfig, err := bundleuri.UploadPackGitConfig(ctx, s.bundleURISink, req.GetRepository())
 	if err != nil {
-		log.AddFields(ctx, log.Fields{"bundle_uri_error": err})
+		if errors.Is(err, bundleuri.ErrBundleNotFound) &&
+			featureflag.AutogenerateBundlesForBundleURI.IsEnabled(ctx) &&
+			s.generateBundles &&
+			s.inflightTracker.GetInProgress(key) > concurrentUploadPackThreshold {
+
+			go func() {
+				ctx, cancel := context.WithTimeout(context.Background(), bundleGenerationTimeout)
+				defer cancel()
+
+				tx, err := s.partitionMgr.Begin(
+					ctx,
+					originalRepo.GetStorageName(),
+					originalRepo.GetRelativePath(),
+					0,
+					storagemgr.TransactionOptions{
+						ReadOnly: true,
+					},
+				)
+				if err != nil {
+					ctxlogrus.Extract(ctx).WithError(err).Error("failed starting transaction")
+				}
+
+				ctx = storagectx.ContextWithTransaction(ctx, tx)
+
+				if err := s.bundleURISink.GenerateOneAtATime(ctx, localrepo.New(
+					s.logger,
+					s.locator,
+					s.gitCmdFactory,
+					s.catfileCache,
+					originalRepo)); err != nil {
+					ctxlogrus.Extract(ctx).WithError(err).Error("generate bundle")
+					if err := tx.Rollback(); err != nil && !errors.Is(err, storagemgr.ErrTransactionAlreadyCommitted) {
+						ctxlogrus.Extract(ctx).WithError(err).Error("failed rolling back transaction")
+					}
+				}
+
+				if err := tx.Commit(ctx); err != nil && !errors.Is(err, storagemgr.ErrTransactionAlreadyCommitted) {
+					ctxlogrus.Extract(ctx).WithError(err).Error("committing transaction")
+				}
+			}()
+		} else if !errors.Is(err, bundleuri.ErrSinkMissing) {
+			log.AddFields(ctx, log.Fields{"bundle_uri_error": err})
+		}
 	} else {
 		gitConfig = append(gitConfig, uploadPackConfig...)
 	}
 
+	var stderr bytes.Buffer
+
 	commandOpts := []git.CmdOpt{
 		git.WithStdin(stdin),
+		git.WithStderr(&stderr),
 		git.WithSetupStdout(),
 		git.WithGitProtocol(s.logger, req),
 		git.WithConfig(gitConfig...),
 		git.WithPackObjectsHookEnv(req.GetRepository(), "http"),
 	}
+
+	s.inflightTracker.IncrementInProgress(key)
+	defer s.inflightTracker.DecrementInProgress(key)
 
 	cmd, err := s.gitCmdFactory.New(ctx, req.GetRepository(), git.Command{
 		Name:  "upload-pack",
@@ -160,5 +237,6 @@ func (s *server) runUploadPack(ctx context.Context, req *gitalypb.PostUploadPack
 	}
 
 	s.logger.WithField("request_sha", fmt.Sprintf("%x", h.Sum(nil))).WithField("response_bytes", respBytes).InfoContext(ctx, "request details")
+
 	return nil, nil
 }
