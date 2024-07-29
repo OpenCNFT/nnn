001d4c64ef17ee55ec2cc832749e744aa9d5f7ce
Sami Hiltunen <shiltunen@gitlab.com>
2024-07-29 18:20:38 +0300

    Remove SharedFile permission
    Gitaly was previously using SharedFile permission in various places.
    Now that we've standardized on a smaller set of permissions and
    removed the last use of the permission with log files, remove the
    permission constant.

--- internal/helper/perm/perm.go
+++ internal/helper/perm/perm.go
@@ -1,17 +1,7 @@
-// Package perm provides constants for file and directory permissions.
-//
-// Note that these permissions are further restricted by the system configured
-// umask.
 package perm
 
 import (
 	"io/fs"
-)
-
-const (
-	// SharedFile is the permission given for a file that may be read outside
-	// of gitaly.
-	SharedFile fs.FileMode = 0o644
 )
 
 // Umask represents a umask that is used to mask mode bits.
