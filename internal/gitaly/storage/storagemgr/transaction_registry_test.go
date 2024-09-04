ceb2006c2b35782143a27b3fe1b9268e9568711b
Sami Hiltunen <shiltunen@gitlab.com>
2024-09-04 19:17:13 +0300

    Use a mock transaction in TransactionRegistry tests
    TransactionRegistry is using the real Transaction type in its test.
    Use a mock instead to remove the dependency so we can move the
    TransactionManager into its own package.

--- internal/gitaly/storage/storagemgr/transaction_registry_test.go
+++ internal/gitaly/storage/storagemgr/transaction_registry_test.go
@@ -10,11 +10,13 @@ import (
 func TestTransactionRegistry(t *testing.T) {
 	registry := NewTransactionRegistry()
 
-	expectedTX1 := &Transaction{}
+	type nilTransaction struct{ storage.Transaction }
+
+	expectedTX1 := &nilTransaction{}
 	txID1 := registry.register(expectedTX1)
 	require.Equal(t, txID1, storage.TransactionID(1))
 
-	expectedTX2 := &Transaction{}
+	expectedTX2 := &nilTransaction{}
 	txID2 := registry.register(expectedTX2)
 	require.Equal(t, txID2, storage.TransactionID(2))
 
