23479c0d4889ea09648d9c104b4e02d599496247
Sami Hiltunen <shiltunen@gitlab.com>
2024-09-05 15:46:20 +0300

    Implement a storage factory
    This commit implements a new factory type that can build storages.
    This way we don't need to drill down dependencies of storages
    through other layers but can instead just encapsulate the knowledge
    and dependencies needed in the factory, and inject that instead.

---
+++ internal/gitaly/storage/storagemgr/factory.go
@@ -0,0 +1,38 @@
+package storagemgr
+
+import (
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage/keyvalue/databasemgr"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage/node"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/log"
+)
+
+// Factory is a factory type that can instantiate new storages.
+type Factory struct {
+	logger           log.Logger
+	dbMgr            *databasemgr.DBManager
+	partitionFactory PartitionFactory
+	metrics          *Metrics
+}
+
+// NewFactory returns a new Factory.
+func NewFactory(
+	logger log.Logger,
+	dbMgr *databasemgr.DBManager,
+	partitionFactory PartitionFactory,
+	metrics *Metrics,
+) Factory {
+	return Factory{
+		logger:           logger,
+		dbMgr:            dbMgr,
+		partitionFactory: partitionFactory,
+		metrics:          metrics,
+	}
+}
+
+// New returns a new Storage.
+func (f Factory) New(name, path string, consumer storage.LogConsumer) (node.Storage, error) {
+	return NewStorageManager(
+		f.logger, name, path, f.dbMgr, f.partitionFactory, consumer, f.metrics,
+	)
+}
