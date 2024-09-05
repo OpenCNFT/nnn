90367efea5f1c917535ff6d9d58a47371af244be
Justin Tobler <jtobler@gitlab.com>
2024-09-05 16:03:52 -0500

    featureflag: Remove Git 2.46 flag
    Now that the Git 2.46 feature flag has been rolled out to production,
    remove the flag so that the new version is always used.

--- internal/featureflag/ff_git_v246.go
+++
@@ -1,9 +0,0 @@
-package featureflag
-
-// GitV246 enables the use Git v2.46.
-var GitV246 = NewFeatureFlag(
-	"git_v246",
-	"v17.4.0",
-	"https://gitlab.com/gitlab-org/gitaly/-/issues/6302",
-	false,
-)
