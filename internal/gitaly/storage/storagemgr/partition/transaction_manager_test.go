7dadcdf51f07ff7e931edab56db91bc37e90e8a1
Sami Hiltunen <shiltunen@gitlab.com>
2024-09-26 16:47:54 +0300

    Record an execution trace in BenchmarkTransactionManager
    When looking to understand the result of the benchmark, it helps
    a lot to have an execution trace at hand. Output an execution trace
    when BenchmarkTransactionManager is ran.

--- internal/gitaly/storage/storagemgr/partition/transaction_manager_test.go
+++ internal/gitaly/storage/storagemgr/partition/transaction_manager_test.go
@@ -11,6 +11,7 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"runtime/trace"
 	"strconv"
 	"strings"
 	"sync"
@@ -2392,6 +2393,13 @@ func BenchmarkTransactionManager(b *testing.B) {
 			tc.transactionSize,
 		)
 		b.Run(desc, func(b *testing.B) {
+			traceFile, err := os.OpenFile("trace.bin", os.O_CREATE|os.O_TRUNC|os.O_RDWR, os.ModePerm)
+			require.NoError(b, err)
+			defer traceFile.Close()
+
+			require.NoError(b, trace.Start(traceFile))
+			defer trace.Stop()
+
 			ctx := testhelper.Context(b)
 
 			cfg := testcfg.Build(b)
