66a15da29f1e2d85037ac8aa9809bc8935e26182
Sami Hiltunen <shiltunen@gitlab.com>
2024-09-28 14:22:46 +0300

    Write and read directly from cat-file
    checkObjects is currently using multiple a goroutines to write
    input to cat-file, wait for cat-file, and read output from cat-file.
    It's using io.Pipe to pass input and output between these goroutines.
    This is inefficient. io.Pipe is a synchronous in-memory structure.
    Passing an io.Pipe as stdin to the command leads it to launch a
    goroutine that copies from the in-memory pipe to the actual stdin
    pipe of the command.
    
    Stop using io.Pipe and instead write directly to the pipe that is
    connected to the cat-file command's stdin. We do this by using
    WithSetupStdin that leads to `os/exec` setting up an `os.Pipe` and
    passing that directly to the command. Doing so, we can also reduce
    the number of goroutines used as there's no need for separate
    goroutine to copy the input written to the pipe to the command.
    We also move the reader to the main goroutine as it would anyway wait
    for the output to be read.

--- internal/gitaly/storage/storagemgr/partition/check_objects.go
+++ internal/gitaly/storage/storagemgr/partition/check_objects.go
@@ -4,8 +4,8 @@ import (
 	"bufio"
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
-	"io"
 	"runtime/trace"
 	"strings"
 
@@ -13,84 +13,92 @@ import (
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git/gitcmd"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git/localrepo"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/structerr"
-	"golang.org/x/sync/errgroup"
 )
 
-func checkObjects(ctx context.Context, repository *localrepo.Repo, revisions []git.Revision, callback func(revision git.Revision, objectID git.ObjectID) error) error {
+func checkObjects(ctx context.Context, repository *localrepo.Repo, revisions []git.Revision, callback func(revision git.Revision, objectID git.ObjectID) error) (returnedErr error) {
 	defer trace.StartRegion(ctx, "checkObjects").End()
 
-	eg, ctx := errgroup.WithContext(ctx)
-	revisionReader, revisionWriter := io.Pipe()
-	eg.Go(func() (returnedErr error) {
-		defer revisionWriter.CloseWithError(returnedErr)
-
-		input := bufio.NewWriter(revisionWriter)
-		for _, revision := range revisions {
-			if _, err := fmt.Fprintln(input, revision.String()); err != nil {
-				return fmt.Errorf("write revision: %w", err)
-			}
-		}
-
-		if err := input.Flush(); err != nil {
-			return fmt.Errorf("flush: %w", err)
-		}
-
-		return nil
-	})
-
-	resultReader, resultWriter := io.Pipe()
-	eg.Go(func() (returnedErr error) {
-		defer revisionReader.CloseWithError(returnedErr)
-		defer resultWriter.CloseWithError(returnedErr)
-
-		var stderr bytes.Buffer
-		if err := repository.ExecAndWait(ctx,
-			gitcmd.Command{
-				Name: "cat-file",
-				Flags: []gitcmd.Option{
-					gitcmd.Flag{Name: "--batch-check=%(objectname)"},
-					gitcmd.Flag{Name: "--buffer"},
-				},
+	var stderr bytes.Buffer
+	cmd, err := repository.Exec(ctx,
+		gitcmd.Command{
+			Name: "cat-file",
+			Flags: []gitcmd.Option{
+				gitcmd.Flag{Name: "--batch-check=%(objectname)"},
+				gitcmd.Flag{Name: "--buffer"},
 			},
-			gitcmd.WithStdin(revisionReader),
-			gitcmd.WithStdout(resultWriter),
-			gitcmd.WithStderr(&stderr),
-		); err != nil {
-			return structerr.New("cat-file: %w", err).WithMetadata("stderr", stderr.String())
+		},
+		gitcmd.WithSetupStdin(),
+		gitcmd.WithSetupStdout(),
+		gitcmd.WithStderr(&stderr),
+	)
+	if err != nil {
+		return structerr.New("exec cat-file: %w", err)
+	}
+
+	writerErr := make(chan error, 1)
+	defer func() {
+		// Close stdin so the writer returns in case we encountered an error while reading.
+		// Ignore the error as we only care about unblocking the writer here.
+		_ = cmd.Stdin().Close()
+
+		// Wait for the writer goroutine to return and capture its error.
+		if err := <-writerErr; err != nil {
+			returnedErr = errors.Join(returnedErr, fmt.Errorf("writer: %w", err))
 		}
 
-		return nil
-	})
-
-	eg.Go(func() (returnedErr error) {
-		defer resultReader.CloseWithError(returnedErr)
-
-		objectHash, err := repository.ObjectHash(ctx)
-		if err != nil {
-			return fmt.Errorf("object hash: %w", err)
+		// Wait for the command to exit.
+		if err := cmd.Wait(); err != nil {
+			returnedErr = errors.Join(returnedErr, structerr.New("cat-file: %w", err).WithMetadata("stderr", stderr.String()))
 		}
+	}()
 
-		scanner := bufio.NewScanner(resultReader)
-		for i := 0; scanner.Scan(); i++ {
-			oid := objectHash.ZeroOID
-			if rawOID, isMissing := strings.CutSuffix(scanner.Text(), " missing"); !isMissing {
-				oid, err = objectHash.FromHex(rawOID)
-				if err != nil {
-					return fmt.Errorf("parse object id: %w", err)
+	go func() {
+		writerErr <- func() (returnedErr error) {
+			// Close stdin to signal to cat-file there's no more input.
+			defer func() {
+				if err := cmd.Stdin().Close(); err != nil {
+					returnedErr = errors.Join(returnedErr, fmt.Errorf("close stdin: %w", err))
+				}
+			}()
+
+			input := bufio.NewWriter(cmd)
+			for _, revision := range revisions {
+				if _, err := fmt.Fprintln(input, revision.String()); err != nil {
+					return fmt.Errorf("write revision: %w", err)
 				}
 			}
 
-			if err := callback(revisions[i], oid); err != nil {
-				return fmt.Errorf("callback: %w", err)
+			if err := input.Flush(); err != nil {
+				return fmt.Errorf("flush: %w", err)
+			}
+
+			return nil
+		}()
+	}()
+
+	objectHash, err := repository.ObjectHash(ctx)
+	if err != nil {
+		return fmt.Errorf("object hash: %w", err)
+	}
+
+	scanner := bufio.NewScanner(cmd)
+	for i := 0; scanner.Scan(); i++ {
+		oid := objectHash.ZeroOID
+		if rawOID, isMissing := strings.CutSuffix(scanner.Text(), " missing"); !isMissing {
+			oid, err = objectHash.FromHex(rawOID)
+			if err != nil {
+				return fmt.Errorf("parse object id: %w", err)
 			}
 		}
 
-		if err := scanner.Err(); err != nil {
-			return fmt.Errorf("scanning cat-file output: %w", err)
+		if err := callback(revisions[i], oid); err != nil {
+			return fmt.Errorf("callback: %w", err)
 		}
+	}
 
-		return nil
-	})
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("scanning cat-file output: %w", err)
+	}
 
-	return eg.Wait()
+	return nil
 }
