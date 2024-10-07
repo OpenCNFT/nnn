3366e5b127dfa81375d52b22e02249ec596e5d66
James Liu <jliu@gitlab.com>
2024-10-07 15:23:38 +1100

    check: Ensure unpacked binaries are cleaned up
    The `gitaly check` command instantiates a GitLabHookManager to verify
    that Gitaly is able to access Rails' /internal/check endpoint. The hook
    manager requires an ExecCommandFactory which requires Git binaries to be
    configured properly. Due to this, we must unpack the embedded Git
    binaries into `cfg.RuntimeDir` when this command is executed.
    
    There were three problems with the implementation:
    
    - We didn't clean up the runtime directory, so multiple invocations of
      the command would fail when it tries to unpack binaries into an
      existing location.
    - We use `cfg.RuntimeDir` directly as configured in config.toml. We
      should instead be creating subdirectories like we do elsewhere.
    - We didn't clean up leftover runtime directories, which may exist if
      a previous process crashed.
    
    Address these problems by unpacking binaries into a subdirectory of
    `cfg.RuntimeDir`, and ensure the subdirectory is cleaned up at the
    beginning and end of execution. We use `config.SetupRuntimeDirectory()`
    which is the same mechanism used by the sever initialisation.

--- cmd/gitaly/check_test.go
+++ cmd/gitaly/check_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"io"
+	"os"
 	"os/exec"
 	"testing"
 
@@ -45,6 +47,15 @@ func TestCheckOK(t *testing.T) {
 		},
 	}), testcfg.WithDisableBundledGitSymlinking())
 	testcfg.BuildGitaly(t, cfg)
+
+	// BuildGitaly() above will manipulate cfg.RuntimeDir to point to the Gitaly test server's runtime dir, which
+	// will contain a `sock.d` directory. When this exists, PruneOldGitalyProcessDirectories() will complain as
+	// it's not recognised as a directory of the form `gitaly-<pid>`, and it emits a warning on stderr which fails
+	// one of the test assertions below. This is okay however, and only happens during tests.
+	//
+	// We reset `cfg.RuntimeDir` to an empty string which allows the subcommand to configure a fresh runtime
+	// directory independent of the one configured above.
+	cfg.RuntimeDir = ""
 
 	configPath := testcfg.WriteTemporaryGitalyConfigFile(t, cfg)
 
@@ -91,4 +102,39 @@ func TestCheckBadCreds(t *testing.T) {
 	require.Contains(t, stdout.String(), `Checking GitLab API access: FAILED`)
 	require.Contains(t, stderr.String(), "HTTP GET to GitLab endpoint /check failed: authorization failed")
 	require.Regexp(t, `.* level=error msg="Internal API error" .* error="authorization failed" method=GET pid=[0-9]+ status=401 url="http://127.0.0.1:[0-9]+/api/v4/internal/check"`, stderr.String())
+}
+
+func TestCheckAuxiliaryBinaries(t *testing.T) {
+	user, password, serverURL, cleanup := createTestServer(t)
+	defer cleanup()
+
+	runtimeDir, err := os.MkdirTemp(os.TempDir(), "")
+	require.NoError(t, err)
+
+	cfg := testcfg.Build(t, testcfg.WithBase(config.Cfg{
+		Gitlab: config.Gitlab{
+			URL: serverURL,
+			HTTPSettings: config.HTTPSettings{
+				User:     user,
+				Password: password,
+			},
+			SecretFile: gitlab.WriteShellSecretFile(t, testhelper.TempDir(t), "the secret"),
+		},
+		RuntimeDir: runtimeDir,
+	}), testcfg.WithDisableBundledGitSymlinking())
+	testcfg.BuildGitaly(t, cfg)
+
+	configPath := testcfg.WriteTemporaryGitalyConfigFile(t, cfg)
+
+	cmd := exec.Command(cfg.BinaryPath("gitaly"), "check", configPath)
+	require.NoError(t, cmd.Run())
+
+	// When Gitaly starts, it needs to unpack embedded binaries to the configured runtime directory.
+	// It is expected that these binaries are cleanup when the program completes.
+	f, err := os.Open(runtimeDir)
+	require.NoError(t, err)
+	defer f.Close()
+
+	_, err = f.Readdirnames(1)
+	require.ErrorIs(t, err, io.EOF)
 }
