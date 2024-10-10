4f800aa7daed5f1a2b26699490459a565de88f34
Justin Tobler <jtobler@gitlab.com>
2024-10-10 17:50:18 -0500

    localrepo: Disable `FetchBundle()` fsck
    Starting in Git version 2.46.0, executing git-fetch(1) on a bundle
    performs fsck checks when `transfer.fsckObjects` is enabled. Prior to
    this, this configuration was always ignored and fsck checks were not
    run. Unfortunately, fsck message severity configuration is ignored by
    Git only for bundle fetches.
    
    Until this is supported by Git, disable `transfer.fsckObjects` so
    bundles containing fsck errors can continue to be fetched. This matches
    behavior prior to Git version 2.46.0.

--- internal/git/localrepo/bundle_test.go
+++ internal/git/localrepo/bundle_test.go
@@ -333,19 +333,24 @@ func TestRepo_FetchBundle(t *testing.T) {
 
 				// Write a tree object that has the same path twice. When git-fetch(1) executes with
 				// `transfer.fsckObjects=true`, it is expected that this objects causes the fetch to fail.
+				//
+				// The git-fetch(1) performed when fetching bundles is configured to override
+				// `transfer.fsckObjects` to `false`. Therefore, fsck errors are ignored.
 				treeID := gittest.WriteTree(t, cfg, sourceRepoPath, []gittest.TreeEntry{
 					{Path: "duplicate", Mode: "100644", Content: "foo"},
 					{Path: "duplicate", Mode: "100644", Content: "bar"},
 				})
 
-				gittest.WriteCommit(t, cfg, sourceRepoPath, gittest.WithBranch(git.DefaultBranch), gittest.WithTree(treeID))
+				mainOid := gittest.WriteCommit(t, cfg, sourceRepoPath, gittest.WithBranch(git.DefaultBranch), gittest.WithTree(treeID))
 
 				return testSetup{
-					reader: createBundle(t, cfg, sourceRepoPath),
+					reader:          createBundle(t, cfg, sourceRepoPath),
+					expectedHeadRef: git.DefaultRef,
+					expectedRefs: []git.Reference{
+						git.NewReference(git.DefaultRef, mainOid),
+					},
 				}
 			},
-			expectedErrAs:       FetchFailedError{},
-			expectedErrContains: "duplicateEntries: contains duplicate file entries",
 		},
 	} {
 		tc := tc
