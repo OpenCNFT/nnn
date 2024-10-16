8ab0275feb12cdd03e37437939e97d1cfee5d799
Karthik Nayak <knayak@gitlab.com>
2024-10-16 21:13:28 +0200

    featureflag: Remove featureflag Git v2.47
    The new Git version of v2.47 was added behind a featureflag. Now that we
    have rolled out the feature flag, we can remove the flag.

--- internal/featureflag/ff_git_v247.go
+++
@@ -1,9 +0,0 @@
-package featureflag
-
-// GitV247 enables the use Git v2.47.
-var GitV247 = NewFeatureFlag(
-	"git_v247",
-	"v17.5.0",
-	"https://gitlab.com/gitlab-org/gitaly/-/issues/6419",
-	true,
-)
