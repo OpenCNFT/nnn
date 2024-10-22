330c7f483041649d7bd767e90171e01e951fba7f
Quang-Minh Nguyen <qmnguyen@gitlab.com>
2024-10-22 17:10:33 +0700

    storagemgr: Add LSN to test hooks
    The transaction manager has a set of test hooks. They are triggered
    before performing certain critical actions. The test suite uses those
    hooks to inject undesirable events, such as crashes or unexpected
    manager closings. Currently, they don't accept any arguments since those
    events are applied widely.
    
    In some later commits, the test suite needs to trigger hooks
    conditionally. This is due to the fact that Raft injects some internal
    log entries, such as config changes or empty log entries, for
    verification. The tests fail prematurely if the hook is triggered while
    applying those entries. While the transaction manager resumes the
    processing successfully afterward, crashing Raft's internal log entries
    should be covered elsewhere. The existing tests only cover the crashing
    while committing "normal" log entries.
    
    This commit adds LSN to test hooks for that use case.

--- internal/gitaly/storage/storagemgr/partition/transaction_manager.go
+++ internal/gitaly/storage/storagemgr/partition/transaction_manager.go
@@ -1045,10 +1045,10 @@ type TransactionManager struct {
 
 type testHooks struct {
 	beforeInitialization      func()
-	beforeAppendLogEntry      func()
-	beforeApplyLogEntry       func()
-	beforeStoreAppliedLSN     func()
-	beforeDeleteLogEntryFiles func()
+	beforeAppendLogEntry      func(storage.LSN)
+	beforeApplyLogEntry       func(storage.LSN)
+	beforeStoreAppliedLSN     func(storage.LSN)
+	beforeDeleteLogEntryFiles func(storage.LSN)
 	beforeRunExiting          func()
 }
 
@@ -1097,10 +1097,10 @@ func NewTransactionManager(
 
 		testHooks: testHooks{
 			beforeInitialization:      func() {},
-			beforeAppendLogEntry:      func() {},
-			beforeApplyLogEntry:       func() {},
-			beforeStoreAppliedLSN:     func() {},
-			beforeDeleteLogEntryFiles: func() {},
+			beforeAppendLogEntry:      func(storage.LSN) {},
+			beforeApplyLogEntry:       func(storage.LSN) {},
+			beforeStoreAppliedLSN:     func(storage.LSN) {},
+			beforeDeleteLogEntryFiles: func(storage.LSN) {},
 			beforeRunExiting:          func() {},
 		},
 	}
@@ -3538,9 +3538,9 @@ func (mgr *TransactionManager) appendLogEntry(ctx context.Context, objectDepende
 		return fmt.Errorf("synchronizing WAL directory: %w", err)
 	}
 
-	mgr.testHooks.beforeAppendLogEntry()
-
 	nextLSN := mgr.appendedLSN + 1
+	mgr.testHooks.beforeAppendLogEntry(nextLSN)
+
 	// Move the log entry from the staging directory into its place in the log.
 	destinationPath := walFilesPathForLSN(mgr.stateDirectory, nextLSN)
 	if err := os.Rename(logEntryPath, destinationPath); err != nil {
@@ -3600,7 +3600,7 @@ func (mgr *TransactionManager) applyLogEntry(ctx context.Context, lsn storage.LS
 	delete(mgr.snapshotLocks, previousLSN)
 	mgr.mutex.Unlock()
 
-	mgr.testHooks.beforeApplyLogEntry()
+	mgr.testHooks.beforeApplyLogEntry(lsn)
 
 	if err := applyOperations(ctx, safe.NewSyncer().Sync, mgr.storagePath, walFilesPathForLSN(mgr.stateDirectory, lsn), logEntry, mgr.db); err != nil {
 		return fmt.Errorf("apply operations: %w", err)
@@ -3615,7 +3615,7 @@ func (mgr *TransactionManager) applyLogEntry(ctx context.Context, lsn storage.LS
 		}
 	}
 
-	mgr.testHooks.beforeStoreAppliedLSN()
+	mgr.testHooks.beforeStoreAppliedLSN(lsn)
 	if err := mgr.storeAppliedLSN(lsn); err != nil {
 		return fmt.Errorf("set applied LSN: %w", err)
 	}
