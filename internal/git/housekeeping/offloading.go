6439bb7e1eba24107b9362c0533cf4ad1db2f116
Eric Ju <eju@gitlab.com>
2024-11-01 14:55:39 -0400

    housekeeping: Add offloading control flow to housekeeping
    This commits add OffloadRepository and RehydrateRepository interface
    to the housekeeping manager. It also adds bunch of helper functions
    to modularize the offloading/rehydrating logic. The actual
    implementation is left out, since we just want to focus on the control
    flow first.

---
+++ internal/git/housekeeping/offloading.go
@@ -0,0 +1,51 @@
+package housekeeping
+
+import (
+	"context"
+
+	"gitlab.com/gitlab-org/gitaly/v16/internal/git/gitcmd"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/git/housekeeping/config"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/git/localrepo"
+)
+
+// SetOffloadingGitConfig add a new promisor remote in the git config of the offloaded repository.
+func SetOffloadingGitConfig(ctx context.Context, repo *localrepo.Repo) error {
+	return nil
+}
+
+// ResetOffloadingGitConfig remote the promisor remote from the offloaded repository.
+func ResetOffloadingGitConfig(ctx context.Context, repo *localrepo.Repo) error {
+	return nil
+}
+
+// AddOffloadTransientAlternate add an alternate in the offloaded repository. This alternate
+// serves as a cache for the object that download back temporarily.
+func AddOffloadTransientAlternate(ctx context.Context, repo *localrepo.Repo) error {
+	return nil
+}
+
+// RemoveOffloadTransientAlternate removes the transient alternate for offloading from the
+// git alternate file.
+func RemoveOffloadTransientAlternate(ctx context.Context, repo *localrepo.Repo) error {
+	return nil
+}
+
+// GetOffloadingRepackOptions returns the flags for  `git-repack(1)` command
+func GetOffloadingRepackOptions(filter string, filterTo string) (config.RepackObjectsConfig, []gitcmd.Option) {
+	return config.RepackObjectsConfig{}, []gitcmd.Option{}
+}
+
+// UploadToOffloadingStorage uploads the files to the offloading storage
+func UploadToOffloadingStorage(ctx context.Context, bucket string, bucketPrefix string, uploadFrom string) error {
+	return nil
+}
+
+// DownloadOffloadingStorage downloads the files from offloading storage
+func DownloadOffloadingStorage(ctx context.Context, bucket string) error {
+	return nil
+}
+
+// RemoveFromOffloadingStorage remove files from offloading storage
+func RemoveFromOffloadingStorage() error {
+	return nil
+}
