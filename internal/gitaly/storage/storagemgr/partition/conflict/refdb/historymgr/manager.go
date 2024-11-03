5ccd38d35e08ba75e34316621fe8dffb5a451737
Sami Hiltunen <shiltunen@gitlab.com>
2024-11-03 12:37:00 +0100

    Implement manager for repository histories in partition
    In the previous commit, we implemented a history that keeps track
    of changes in a given repository. Each partition may contain more
    than one repository though, so we need a type to manage multiple
    histories.
    
    Histories need to be created when the repository receives its first
    writes. Transactions need to be evicted from the histories once they
    are no longer needed for conflict checking to release the memory
    associated with them.
    
    This commit implements a history manager that keeps track of the
    reference histories in a partition. TransactionManager can then use
    this type to get the history for a correct repository, and apply
    changes to it. The type also tracks which history a transaction at
    a given LSN modified and routes the evictions to the correct history.
    This way transaction manager can simply call evict on the correct LSN,
    and the history manager handles the complexity of dropping the now
    unneeded state.

---
+++ internal/gitaly/storage/storagemgr/partition/conflict/refdb/historymgr/manager.go
@@ -0,0 +1,119 @@
+package historymgr
+
+import (
+	"context"
+	"runtime/trace"
+
+	"gitlab.com/gitlab-org/gitaly/v16/internal/git"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage/storagemgr/partition/conflict/refdb"
+)
+
+// Transaction performs an atomic update against a repository's history under a single LSN.
+type Transaction struct {
+	historyTX *refdb.Transaction
+	commit    func(lsn storage.LSN)
+}
+
+// ApplyUpdates updates the history. See the documentation on the implementation of History.
+func (tx *Transaction) ApplyUpdates(updates git.ReferenceUpdates) ([]refdb.UnexpectedOldValueError, error) {
+	return tx.historyTX.ApplyUpdates(updates)
+}
+
+// Commit commits the transaction.
+func (tx *Transaction) Commit(lsn storage.LSN) {
+	tx.historyTX.Commit(lsn)
+	tx.commit(lsn)
+}
+
+// Manager keeps track of the reference histories of all of the repositories
+// in a partition.
+//
+// Each partition may have multiple repositories which also means multiple
+// reference stores. The reference stores are independent from each other, and
+// changes in a given repository do not conflict with another. Manager stores
+// the histories associated with each repository. It routes transactions to the
+// correct history for a given relative path. It also keeps track of which history
+// was modified by a given LSN. It then routes LSN evictions to the correct history
+// as well. Once a history becomes empty through evictions, state related to it is
+// dropped.
+type Manager struct {
+	// historyByRelativePath stores the reference history associated of each repository
+	// under its relative path.
+	historyByRelativePath map[string]*refdb.History
+	// relativePathsByLSN stores the relative path that was updated by a given LSN. This
+	// is used to figure out which history to evict the LSN.
+	relativePathsByLSN map[storage.LSN]string
+	// lsnByRelativePath stores each LSN keyed by the relative path that was updated by it.
+	// This is used to drop histories that have had all their writes evicted already.
+	lsnByRelativePath map[string]map[storage.LSN]struct{}
+}
+
+// New returns a new Manager.
+func New() *Manager {
+	return &Manager{
+		historyByRelativePath: map[string]*refdb.History{},
+		relativePathsByLSN:    map[storage.LSN]string{},
+		lsnByRelativePath:     map[string]map[storage.LSN]struct{}{},
+	}
+}
+
+// Begin returns a new transaction to modify a repository's history. Changes are persisted
+// when the transaction is committed. The transaction can be discarded if it should not
+// commit.
+func (mgr *Manager) Begin(relativePath string, zeroOID git.ObjectID) *Transaction {
+	history := mgr.historyByRelativePath[relativePath]
+	if history == nil {
+		// There was no existing history for this repository. Create one as this is
+		// the first change to it.
+		history = refdb.NewHistory(zeroOID)
+	}
+
+	return &Transaction{
+		historyTX: history.Begin(),
+		commit: func(lsn storage.LSN) {
+			// Update the indexes.
+			mgr.historyByRelativePath[relativePath] = history
+			mgr.relativePathsByLSN[lsn] = relativePath
+			lsns := mgr.lsnByRelativePath[relativePath]
+			if lsns == nil {
+				lsns = map[storage.LSN]struct{}{}
+				mgr.lsnByRelativePath[relativePath] = lsns
+			}
+			lsns[lsn] = struct{}{}
+		},
+	}
+}
+
+// EvictLSN drops state associated with the given LSN.
+func (mgr *Manager) EvictLSN(lsn storage.LSN) {
+	// Check which history this LSN modified.
+	relativePath := mgr.relativePathsByLSN[lsn]
+	if relativePath == "" {
+		// The LSN did not update a history. The transaction at the LSN
+		// didn't update any references.
+		return
+	}
+
+	mgr.historyByRelativePath[relativePath].Evict(lsn)
+
+	// Remove the evicted LSN from the indexes.
+	delete(mgr.relativePathsByLSN, lsn)
+	delete(mgr.lsnByRelativePath[relativePath], lsn)
+	if len(mgr.lsnByRelativePath[relativePath]) == 0 {
+		// If the history has no further writes, drop it entirely.
+		delete(mgr.historyByRelativePath, relativePath)
+		delete(mgr.lsnByRelativePath, relativePath)
+	}
+}
+
+// EvictRepository drops state associated with a given repository.
+func (mgr *Manager) EvictRepository(ctx context.Context, relativePath string) {
+	defer trace.StartRegion(ctx, "EvictRepository").End()
+
+	for lsn := range mgr.lsnByRelativePath[relativePath] {
+		delete(mgr.relativePathsByLSN, lsn)
+	}
+	delete(mgr.lsnByRelativePath, relativePath)
+	delete(mgr.historyByRelativePath, relativePath)
+}
---
+++ internal/gitaly/storage/storagemgr/partition/conflict/refdb/historymgr/manager_test.go
@@ -0,0 +1,289 @@
+package historymgr
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/git"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/git/gittest"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage"
+)
+
+func TestManager(t *testing.T) {
+	zeroOID := gittest.DefaultObjectHash.ZeroOID
+
+	requireState := func(
+		t *testing.T,
+		mgr *Manager,
+		expectedHistoryByRelativePath map[string]struct{},
+		expectedRelativePathsByLSN map[storage.LSN]string,
+		expectedLSNsByRelativePath map[string]map[storage.LSN]struct{},
+	) {
+		t.Helper()
+
+		actualHistoryByRelativePath := map[string]struct{}{}
+		for key := range mgr.historyByRelativePath {
+			actualHistoryByRelativePath[key] = struct{}{}
+		}
+
+		require.Equal(t, expectedHistoryByRelativePath, actualHistoryByRelativePath)
+		require.Equal(t, expectedRelativePathsByLSN, mgr.relativePathsByLSN)
+		require.Equal(t, expectedLSNsByRelativePath, mgr.lsnByRelativePath)
+	}
+
+	t.Run("no changes before transaction commits", func(t *testing.T) {
+		mgr := New()
+
+		tx := mgr.Begin("relative-path-1", zeroOID)
+
+		// If the transaction is not committed, the state should not change.
+		require.Equal(t, New(), mgr)
+
+		failedUpdates, err := tx.ApplyUpdates(git.ReferenceUpdates{"refs/heads/main": {NewOID: "oid-1"}})
+		require.NoError(t, err)
+		require.Nil(t, failedUpdates)
+		tx.Commit(1)
+
+		// Committed transaction records the change.
+		requireState(t, mgr,
+			map[string]struct{}{
+				"relative-path-1": {},
+			},
+			map[storage.LSN]string{
+				1: "relative-path-1",
+			},
+			map[string]map[storage.LSN]struct{}{
+				"relative-path-1": {1: {}},
+			},
+		)
+	})
+
+	t.Run("reuses existing histories for the same repository", func(t *testing.T) {
+		mgr := New()
+
+		tx := mgr.Begin("relative-path-1", zeroOID)
+		failedUpdates, err := tx.ApplyUpdates(git.ReferenceUpdates{"refs/heads/main": {NewOID: "oid-1"}})
+		require.NoError(t, err)
+		require.Nil(t, failedUpdates)
+		tx.Commit(1)
+
+		// If we start a second transaction against the same relative path, it should
+		// reuse the existing history.
+		existingHistory := mgr.historyByRelativePath["relative-path-1"]
+
+		tx = mgr.Begin("relative-path-1", zeroOID)
+		failedUpdates, err = tx.ApplyUpdates(git.ReferenceUpdates{"refs/heads/main": {OldOID: "oid-1", NewOID: "oid-2"}})
+		require.NoError(t, err)
+		require.Nil(t, failedUpdates)
+		tx.Commit(2)
+
+		require.Same(t, existingHistory, mgr.historyByRelativePath["relative-path-1"])
+		requireState(t, mgr,
+			map[string]struct{}{
+				"relative-path-1": {},
+			},
+			map[storage.LSN]string{
+				1: "relative-path-1",
+				2: "relative-path-1",
+			},
+			map[string]map[storage.LSN]struct{}{
+				"relative-path-1": {1: {}, 2: {}},
+			},
+		)
+	})
+
+	t.Run("uses different histories for different repositories", func(t *testing.T) {
+		mgr := New()
+
+		// Starting a transactions against different relative paths should target different
+		// histories.
+		tx := mgr.Begin("relative-path-1", zeroOID)
+		failedUpdates, err := tx.ApplyUpdates(git.ReferenceUpdates{"refs/heads/main": {NewOID: "oid-1"}})
+		require.NoError(t, err)
+		require.Nil(t, failedUpdates)
+		tx.Commit(1)
+
+		tx = mgr.Begin("relative-path-2", zeroOID)
+		failedUpdates, err = tx.ApplyUpdates(git.ReferenceUpdates{"refs/heads/main": {NewOID: "oid-2"}})
+		require.NoError(t, err)
+		require.Nil(t, failedUpdates)
+		tx.Commit(2)
+
+		require.NotSame(t, mgr.historyByRelativePath["relative-path-1"], mgr.historyByRelativePath["relative-path-2"])
+		requireState(t, mgr,
+			map[string]struct{}{
+				"relative-path-1": {},
+				"relative-path-2": {},
+			},
+			map[storage.LSN]string{
+				1: "relative-path-1",
+				2: "relative-path-2",
+			},
+			map[string]map[storage.LSN]struct{}{
+				"relative-path-1": {1: {}},
+				"relative-path-2": {2: {}},
+			},
+		)
+	})
+
+	t.Run("evicting LSN without a history does nothing", func(t *testing.T) {
+		mgr := New()
+
+		tx := mgr.Begin("relative-path-1", zeroOID)
+		failedUpdates, err := tx.ApplyUpdates(git.ReferenceUpdates{"refs/heads/main": {NewOID: "oid-1"}})
+		require.NoError(t, err)
+		require.Nil(t, failedUpdates)
+		tx.Commit(1)
+
+		// Evicting non-existent LSN does nothing.
+		mgr.EvictLSN(0)
+		requireState(t, mgr,
+			map[string]struct{}{
+				"relative-path-1": {},
+			},
+			map[storage.LSN]string{
+				1: "relative-path-1",
+			},
+			map[string]map[storage.LSN]struct{}{
+				"relative-path-1": {1: {}},
+			},
+		)
+	})
+
+	t.Run("evict retains non-empty histories", func(t *testing.T) {
+		mgr := New()
+
+		tx := mgr.Begin("relative-path-1", zeroOID)
+		failedUpdates, err := tx.ApplyUpdates(git.ReferenceUpdates{"refs/heads/main": {NewOID: "oid-1"}})
+		require.NoError(t, err)
+		require.Nil(t, failedUpdates)
+		tx.Commit(1)
+
+		tx = mgr.Begin("relative-path-1", zeroOID)
+		failedUpdates, err = tx.ApplyUpdates(git.ReferenceUpdates{"refs/heads/main": {OldOID: "oid-1", NewOID: "oid-2"}})
+		require.NoError(t, err)
+		require.Nil(t, failedUpdates)
+		tx.Commit(2)
+
+		requireState(t, mgr,
+			map[string]struct{}{
+				"relative-path-1": {},
+			},
+			map[storage.LSN]string{
+				1: "relative-path-1",
+				2: "relative-path-1",
+			},
+			map[string]map[storage.LSN]struct{}{
+				"relative-path-1": {1: {}, 2: {}},
+			},
+		)
+
+		// Evicting an LSN keeps the history still around if
+		// there are further writes into it.
+		mgr.EvictLSN(1)
+		requireState(t, mgr,
+			map[string]struct{}{
+				"relative-path-1": {},
+			},
+			map[storage.LSN]string{
+				2: "relative-path-1",
+			},
+			map[string]map[storage.LSN]struct{}{
+				"relative-path-1": {2: {}},
+			},
+		)
+	})
+
+	t.Run("evict drops empty histories", func(t *testing.T) {
+		mgr := New()
+
+		tx := mgr.Begin("relative-path-1", zeroOID)
+		failedUpdates, err := tx.ApplyUpdates(git.ReferenceUpdates{"refs/heads/main": {NewOID: "oid-1"}})
+		require.NoError(t, err)
+		require.Nil(t, failedUpdates)
+		tx.Commit(1)
+
+		tx = mgr.Begin("relative-path-2", zeroOID)
+		failedUpdates, err = tx.ApplyUpdates(git.ReferenceUpdates{"refs/heads/main": {NewOID: "oid-1"}})
+		require.NoError(t, err)
+		require.Nil(t, failedUpdates)
+		tx.Commit(2)
+
+		requireState(t, mgr,
+			map[string]struct{}{
+				"relative-path-1": {},
+				"relative-path-2": {},
+			},
+			map[storage.LSN]string{
+				1: "relative-path-1",
+				2: "relative-path-2",
+			},
+			map[string]map[storage.LSN]struct{}{
+				"relative-path-1": {1: {}},
+				"relative-path-2": {2: {}},
+			},
+		)
+
+		// Evicting an LSN keeps drops the history if there are no
+		// further writes to it.
+		mgr.EvictLSN(1)
+		requireState(t, mgr,
+			map[string]struct{}{
+				"relative-path-2": {},
+			},
+			map[storage.LSN]string{
+				2: "relative-path-2",
+			},
+			map[string]map[storage.LSN]struct{}{
+				"relative-path-2": {2: {}},
+			},
+		)
+
+		mgr.EvictLSN(2)
+		requireState(t, mgr,
+			map[string]struct{}{},
+			map[storage.LSN]string{},
+			map[string]map[storage.LSN]struct{}{},
+		)
+	})
+
+	t.Run("empty transactions are handled the same as others", func(t *testing.T) {
+		mgr := New()
+
+		mgr.Begin("relative-path-1", zeroOID).Commit(1)
+		mgr.Begin("relative-path-1", zeroOID).Commit(2)
+
+		requireState(t, mgr,
+			map[string]struct{}{
+				"relative-path-1": {},
+			},
+			map[storage.LSN]string{
+				1: "relative-path-1",
+				2: "relative-path-1",
+			},
+			map[string]map[storage.LSN]struct{}{
+				"relative-path-1": {1: {}, 2: {}},
+			},
+		)
+
+		mgr.EvictLSN(1)
+		requireState(t, mgr,
+			map[string]struct{}{
+				"relative-path-1": {},
+			},
+			map[storage.LSN]string{
+				2: "relative-path-1",
+			},
+			map[string]map[storage.LSN]struct{}{
+				"relative-path-1": {2: {}},
+			},
+		)
+
+		mgr.EvictLSN(2)
+		requireState(t, mgr,
+			map[string]struct{}{},
+			map[storage.LSN]string{},
+			map[string]map[storage.LSN]struct{}{},
+		)
+	})
+}
