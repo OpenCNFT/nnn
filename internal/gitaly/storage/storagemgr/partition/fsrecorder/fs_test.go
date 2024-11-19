27ddb450f06c79d1a6aeaee1e0121af4aaae5aa9
Sami Hiltunen <shiltunen@gitlab.com>
2024-11-19 16:41:28 +0100

    Introduce a recording file system implementation
    We're about to introduce general file system level conflict checks.
    Using these checks, we'll move all of the operations to be recorded
    outside of the critical section of the transaction manager and only
    perform conflict checking there. We need an easy way to record the
    file system operations a transaction is performing. This commit
    introduces a type that wraps common file system operations so we can
    record them as they are performed. For now, only Mkdir and MkdirAll
    are supported as we'll need them soon.
    
    We'll later expose this type through the Transaction interface. This
    will enable transactions to stage arbitrary file system operations.

---
+++ internal/gitaly/storage/storagemgr/partition/fsrecorder/fs_test.go
@@ -0,0 +1,149 @@
+package fsrecorder
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"syscall"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage/mode"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage/wal"
+	"gitlab.com/gitlab-org/gitaly/v16/proto/go/gitalypb"
+)
+
+func TestFS(t *testing.T) {
+	t.Run("Mkdir", func(t *testing.T) {
+		t.Run("targeting root fails", func(t *testing.T) {
+			f := NewFS(t.TempDir(), wal.NewEntry(t.TempDir()))
+
+			require.Equal(t, newPathEscapesRootError(""), f.Mkdir(""))
+			require.Nil(t, f.entry.Operations())
+		})
+
+		t.Run("escaping root fails", func(t *testing.T) {
+			f := NewFS(t.TempDir(), wal.NewEntry(t.TempDir()))
+
+			const path = "../non-root-path"
+			require.Equal(t, newPathEscapesRootError(path), f.Mkdir(path))
+			require.Nil(t, f.entry.Operations())
+		})
+
+		t.Run("fails if parent does not exist", func(t *testing.T) {
+			f := NewFS(t.TempDir(), wal.NewEntry(t.TempDir()))
+
+			require.ErrorIs(t, f.Mkdir("non-existent/target"), fs.ErrNotExist)
+			require.Nil(t, f.entry.Operations())
+		})
+
+		t.Run("fails if target exists", func(t *testing.T) {
+			f := NewFS(t.TempDir(), wal.NewEntry(t.TempDir()))
+
+			require.NoError(t, os.Mkdir(filepath.Join(f.root, "target"), mode.Directory))
+
+			require.ErrorIs(t, f.Mkdir("target"), fs.ErrExist)
+		})
+
+		t.Run("successfully creates directories", func(t *testing.T) {
+			f := NewFS(t.TempDir(), wal.NewEntry(t.TempDir()))
+
+			require.NoError(t, f.Mkdir("parent"))
+			require.NoError(t, f.Mkdir("parent/child"))
+			require.Equal(t,
+				[]*gitalypb.LogEntry_Operation{
+					{
+						Operation: &gitalypb.LogEntry_Operation_CreateDirectory_{
+							CreateDirectory: &gitalypb.LogEntry_Operation_CreateDirectory{
+								Path: []byte("parent"),
+								Mode: uint32(mode.Directory),
+							},
+						},
+					},
+					{
+						Operation: &gitalypb.LogEntry_Operation_CreateDirectory_{
+							CreateDirectory: &gitalypb.LogEntry_Operation_CreateDirectory{
+								Path: []byte("parent/child"),
+								Mode: uint32(mode.Directory),
+							},
+						},
+					},
+				},
+				f.entry.Operations(),
+			)
+		})
+	})
+
+	t.Run("MkdirAll", func(t *testing.T) {
+		t.Run("targeting root fails", func(t *testing.T) {
+			f := NewFS(t.TempDir(), wal.NewEntry(t.TempDir()))
+
+			require.Equal(t, newPathEscapesRootError(""), f.MkdirAll(""))
+			require.Nil(t, f.entry.Operations())
+		})
+
+		t.Run("escaping root fails", func(t *testing.T) {
+			f := NewFS(t.TempDir(), wal.NewEntry(t.TempDir()))
+
+			const path = "../non-root-path"
+			require.Equal(t, newPathEscapesRootError(path), f.Mkdir(path))
+			require.Nil(t, f.entry.Operations())
+		})
+
+		t.Run("target under a file", func(t *testing.T) {
+			f := NewFS(t.TempDir(), wal.NewEntry(t.TempDir()))
+
+			require.NoError(t, os.WriteFile(filepath.Join(f.root, "file"), nil, mode.File))
+
+			require.ErrorIs(t, f.MkdirAll("file/target"), syscall.ENOTDIR)
+			require.Nil(t, f.entry.Operations())
+		})
+
+		t.Run("target is a file", func(t *testing.T) {
+			f := NewFS(t.TempDir(), wal.NewEntry(t.TempDir()))
+
+			require.NoError(t, os.WriteFile(filepath.Join(f.root, "file"), nil, mode.File))
+
+			require.Equal(t, newTargetIsFileError("file"), f.MkdirAll("file"))
+			require.Nil(t, f.entry.Operations())
+		})
+
+		t.Run("target exists", func(t *testing.T) {
+			f := NewFS(t.TempDir(), wal.NewEntry(t.TempDir()))
+
+			require.NoError(t, os.MkdirAll(filepath.Join(f.root, "parent/target"), mode.Directory))
+
+			require.NoError(t, f.MkdirAll("parent/target"))
+			require.Nil(t, f.entry.Operations())
+		})
+
+		t.Run("successfully creates missing directories", func(t *testing.T) {
+			f := NewFS(t.TempDir(), wal.NewEntry(t.TempDir()))
+
+			require.NoError(t, os.MkdirAll(filepath.Join(f.root, "parent"), mode.Directory))
+
+			require.NoError(t, f.MkdirAll("parent/child/target"))
+			require.Equal(t,
+				[]*gitalypb.LogEntry_Operation{
+					{
+						Operation: &gitalypb.LogEntry_Operation_CreateDirectory_{
+							CreateDirectory: &gitalypb.LogEntry_Operation_CreateDirectory{
+								Path: []byte("parent/child"),
+								Mode: uint32(mode.Directory),
+							},
+						},
+					},
+					{
+						Operation: &gitalypb.LogEntry_Operation_CreateDirectory_{
+							CreateDirectory: &gitalypb.LogEntry_Operation_CreateDirectory{
+								Path: []byte("parent/child/target"),
+								Mode: uint32(mode.Directory),
+							},
+						},
+					},
+				},
+				f.entry.Operations(),
+			)
+		})
+	})
+}
