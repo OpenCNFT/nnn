1ce70ce6ecc40a3778206c25391e28e80e1cb5db
Sami Hiltunen <shiltunen@gitlab.com>
2024-11-21 14:26:45 +0100

    Write references to the snapshot in BenchmarkTransactionManager
    BenchmarkTransactionManager is currently just calling UpdateReferences
    to stage the reference updates. This will soon no longer work as we're
    about to move the reference recording to happen when the updates are
    performed in the snapshot as opposed to replaying them in the
    transaction manager. Prepare for this by actually writing the references
    into the snapshot in BenchmarkTransactionManager.

--- internal/gitaly/storage/storagemgr/partition/testhelper_test.go
+++ internal/gitaly/storage/storagemgr/partition/testhelper_test.go
@@ -1037,15 +1037,15 @@ type transactionTestCase struct {
 	expectedState StateAssertion
 }
 
-func performReferenceUpdates(t *testing.T, ctx context.Context, tx *Transaction, rewrittenRepo gitcmd.RepositoryExecutor, updates git.ReferenceUpdates) error {
+func performReferenceUpdates(tb testing.TB, ctx context.Context, tx storage.Transaction, rewrittenRepo gitcmd.RepositoryExecutor, updates git.ReferenceUpdates) error {
 	updater, err := updateref.New(ctx, rewrittenRepo)
-	require.NoError(t, err)
+	require.NoError(tb, err)
 
-	require.NoError(t, updater.Start())
+	require.NoError(tb, updater.Start())
 	for reference, update := range updates {
-		require.NoError(t, updater.Update(reference, update.NewOID, update.OldOID))
+		require.NoError(tb, updater.Update(reference, update.NewOID, update.OldOID))
 	}
-	require.NoError(t, updater.Commit())
+	require.NoError(tb, updater.Commit())
 
 	return tx.UpdateReferences(ctx, updates)
 }
