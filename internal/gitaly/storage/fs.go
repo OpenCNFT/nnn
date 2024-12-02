8d178bb56b399d02c0c6ac234aa84ffeb701bc76
Sami Hiltunen <shiltunen@gitlab.com>
2024-12-02 14:34:30 +0100

    Implement Link helper in storage
    Implement a Link helper method in storage that creates a hard link
    and records it. This can be used to create hard link inside the
    partition as done on alternate unlink operations.

--- internal/gitaly/storage/fs.go
+++ internal/gitaly/storage/fs.go
@@ -16,6 +16,19 @@ func newTargetIsFileError(path string) error {
 	return structerr.NewFailedPrecondition("target is a file").WithMetadata("path", path)
 }
 
+// Link creates a hard link from source to destination and records the operation.
+func Link(f FS, source, destination string) error {
+	if err := os.Link(filepath.Join(f.Root(), source), filepath.Join(f.Root(), destination)); err != nil {
+		return fmt.Errorf("link: %w", err)
+	}
+
+	if err := f.RecordLink(source, destination); err != nil {
+		return fmt.Errorf("record link: %w", err)
+	}
+
+	return nil
+}
+
 // Mkdir creates a directory at path and records the operation.
 func Mkdir(f FS, path string) error {
 	if err := os.Mkdir(filepath.Join(f.Root(), path), mode.Directory); err != nil {
